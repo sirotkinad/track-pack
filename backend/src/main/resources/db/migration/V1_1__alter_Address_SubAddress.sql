364fa83b73a8d1832129b434286b2f40e0abccb3
sirotkinad <d-sirotkina@mail.ru>
2021-03-09 19:43:20 +0300

    deleted not actual flyway files

--- backend/src/main/resources/db/migration/V1_1__alter_Address_SubAddress.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE Address DROP CONSTRAINT address_subaddress_fkey;
-
-ALTER TABLE SubAddress
-    ADD COLUMN address UUID;
-
-ALTER TABLE SubAddress ADD CONSTRAINT subaddress_address_fkey
-    FOREIGN KEY (address) REFERENCES Address (id);
-
