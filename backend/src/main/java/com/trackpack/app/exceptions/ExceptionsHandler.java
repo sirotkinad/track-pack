862ba9f4917f2fd866f0d5cd1a7237fab5ef085b
Сироткина Дарья <54890189+sirotkinad@users.noreply.github.com>
2021-03-14 18:21:11 +0300

    Created controller layer(#8) (#13)
    * created service layer
    
    * fixed service layer
    
    * added some validation in service layer
    
    * fixed service layer2
    
    * created controller layer
    
    * changed db password
    
    * changed db password

---
+++ backend/src/main/java/com/trackpack/app/exceptions/ExceptionsHandler.java
@@ -0,0 +1,50 @@
+package com.trackpack.app.exceptions;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class ExceptionsHandler extends ResponseEntityExceptionHandler {
+
+    @Data
+    @AllArgsConstructor
+    private static class ErrorResponse{
+        private String message;
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        private List<String> errors;
+
+        public ErrorResponse(String message){
+            this.message = message;
+        }
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException ex){
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers,
+                                                                  HttpStatus status, WebRequest request) {
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), errors);
+        return new ResponseEntity<>(errorResponse, status);
+    }
+
+}
